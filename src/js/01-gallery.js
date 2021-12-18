583770267eb378f4df74fe1e5eeb9b1d5e0ca601
IhorKozloff <ik.eyeshield21@gmail.com>
2021-12-18 16:43:50 +0200

    dgdfg

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+
+
+function createGalleryItemMarkUp (cardsInform) {
+
+return cardsInform.map(item => {
+    return `
+    <a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.original}" alt="${item.description}" />
+    </a>
+    `
+}).join('');
+}; 
+
+galleryEl.innerHTML = `${createGalleryItemMarkUp(galleryItems)}`;
+
+
+
+function onClickImgElement (event) {
+    event.preventDefault()
+};
+
+const galleryFitches = new SimpleLightbox('.gallery a', {captionsData: "alt", captionsPositin: "bottom", captionsDelay: "250",});
+

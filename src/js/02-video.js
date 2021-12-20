fa32b67c4bcc6433c6f82fc8d4263d4bfbf3e84b
IhorKozloff <ik.eyeshield21@gmail.com>
2021-12-20 12:08:55 +0200

    отправка дз на первую проверку

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+    import Player from '@vimeo/player';
+    const throttle = require('lodash.throttle');
 
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+
+    const onPlay = function(data) {
+        player.getCurrentTime().then(function(seconds) {
+            // seconds = the current playback position
+            localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
+          
+        }).catch(function(error) {
+            // an error occurred
+        });
+    };
+    
+   
+    
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+        // seconds = the actual time that the player seeked to
+        
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+    
+            default:
+                // some other error occurred
+                break;
+        }
+    });
+    player.on('timeupdate', throttle((onPlay), 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+const throttle = require('lodash.throttle');
 
+const refs = {
+    feedbackFormEl: document.querySelector('.feedback-form'),
+    feedbackEmail: document.querySelector(`[name="email"]`),
+    feedbackComent: document.querySelector(`[name="message"]`)
+}
+
+returnFromLocalStorage ()
+
+
+function getFeedback () {
+    const dataInform = {
+        email: refs.feedbackEmail.value,
+        message: refs.feedbackComent.value
+    };
+    localStorage.setItem("feedback-form-state", JSON.stringify(dataInform));
+};
+
+function onFormSubmit (event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    const dataOfLocalStorage = localStorage.getItem("feedback-form-state");
+
+if (dataOfLocalStorage) {
+    const parsedData = JSON.parse(dataOfLocalStorage);
+    console.log(parsedData);
+};
+    localStorage.removeItem('feedback-form-state');
+};
+
+
+function returnFromLocalStorage () {
+const dataOfLocalStorage = localStorage.getItem("feedback-form-state");
+
+if (dataOfLocalStorage) {
+    const parsedData = JSON.parse(dataOfLocalStorage);
+    refs.feedbackEmail.value = parsedData.email;
+    refs.feedbackComent.value = parsedData.message;
+}
+};
+
+refs.feedbackFormEl.addEventListener('input', throttle(getFeedback, 500));
+refs.feedbackFormEl.addEventListener('submit', onFormSubmit)
